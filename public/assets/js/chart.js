c11c498246dc4919dab3ee6943cc048727320600
Zennomi-desktop <ngdatuananh@gmail.com>
2021-03-28 13:47:48 +0700

    demo

--- public/assets/js/chart.js
+++ public/assets/js/chart.js
@@ -1,8 +1,36 @@
-var data = {
+var dataRadar = {
     labels: ["Dao động cơ", "Sóng cơ", "Mạch LC", "Điện xoay chiều", "Sóng ánh sáng", "Lớp 11"],
     datasets: [
         {
-            label: "Năng lực",
+            label: "Kỹ năng",
+            fill: true,
+            lineTension: 0.1,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [5, 6, 7, 8, 9, 10],
+            spanGaps: false,
+        }
+    ]
+};
+
+var dataBar = {
+    labels: ["Dao động cơ", "Sóng cơ", "Mạch LC", "Điện xoay chiều", "Sóng ánh sáng", "Lớp 11"],
+    datasets: [
+        {
+            label: "Kỹ năng",
             fill: true,
             lineTension: 0.1,
             backgroundColor: "rgba(75,192,192,0.4)",
@@ -28,7 +56,7 @@ var data = {
 
 var myRadarChart = new Chart(document.getElementById('radarchart'), {
     type: 'radar',
-    data: data,
+    data: dataRadar,
     options: {
         legend: {
             display: false,
@@ -38,6 +66,23 @@ var myRadarChart = new Chart(document.getElementById('radarchart'), {
                 min: 0,
                 max: 10
             }
+        }
+    }
+});
+
+var myBarChart = new Chart(document.getElementById('barchart'), {
+    type: 'horizontalBar',
+    data: dataBar,
+    options: {
+        legend: {
+            display: false,
+        },
+        scales: {
+            xAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
         }
     }
 });
